b61acd23224d5b2f92bfdceccf3a2ffbc42d5353
Vordgi <sasha2822222@gmail.com>
2024-04-11 19:43:01 +0400

    nic-20 update files formatting

--- package/src/lib/get-server-config.ts
+++ package/src/lib/get-server-config.ts
@@ -1,8 +1,8 @@
 import getConfig from "./get-config";
 
 const getServerConfig = async (configFolder?: string) => {
-    const config = await getConfig({ variant: 'server', configFolder });
+    const config = await getConfig({ variant: "server", configFolder });
     return config;
-}
+};
 
 export default getServerConfig;
--- package/src/runtime-config-api.ts
+++ package/src/runtime-config-api.ts
@@ -1,8 +1,8 @@
-import getRuntimeConfig from './lib/get-runtime-config';
+import getRuntimeConfig from "./lib/get-runtime-config";
 
 const runtimeConfig = getRuntimeConfig(process.env.NIMPL_CONFIG_FOLDER);
 
 export const runtimeConfigApi = async () => {
     const config = await runtimeConfig;
     return Response.json(config);
-}
+};
